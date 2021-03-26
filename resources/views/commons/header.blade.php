d8984cc6bd25c69affd077b13d9e5fb481123ef5
EC2 Default User <ec2-user@ip-172-31-26-73.ec2.internal>
2021-03-26 20:45:12 +0000

    「動画登録する」の修正

--- resources/views/commons/header.blade.php
+++ resources/views/commons/header.blade.php
@@ -22,7 +22,6 @@
                 
                     <li class="nav-item">{!! link_to_route('signup', '新規ユーザ登録', [], ['class' => 'nav-link']) !!}</li>
                     <li class="nav-item">{!! link_to_route('login', 'ログイン', [], ['class' => 'nav-link']) !!}</li>
-                    <li class="nav-item">{!! link_to_route('movies.create','動画登録する',['id'=>Auth::id()],['class'=>'nav-link']) !!}</li>
             
                 @endif
                 
